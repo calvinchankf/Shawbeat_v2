59eddc4195ad8be7e5913624ac0f157bb961c042
calvinchankf <chan9118kin@gmail.com>
2013-10-12 14:54:13 +0800

    Delete DownloadCategoryCompleteListener.java

--- src/com/network/DownloadCategoryCompleteListener.java
+++
@@ -1,9 +0,0 @@
-package com.network;
-
-import java.util.List;
-
-import com.shawbeat.Category;
-
-public interface DownloadCategoryCompleteListener {
-	void DownloadCategoryResultCallback(List<Category> s);
-}
